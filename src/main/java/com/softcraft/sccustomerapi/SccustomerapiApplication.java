8d06cb7078424654a6323ef462eec6f9e568c9e1
Andre Proenca <andreproenca@hotmail.com>
2024-11-30 21:16:18 -0400

    first commit

---
+++ src/main/java/com/softcraft/sccustomerapi/SccustomerapiApplication.java
@@ -0,0 +1,13 @@
+package com.softcraft.sccustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccustomerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccustomerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softcraft/sccustomerapi/SccustomerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcraft.sccustomerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccustomerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
